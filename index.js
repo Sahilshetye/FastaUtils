5d9d92d70f71fc58688131cebf6e40c5797b3d51
sahilshetye <Killeronloose2606>
2017-11-13 18:08:28 -0600

    Cleaning up before publishing

--- index.js
+++
@@ -1,7 +0,0 @@
-// We are  using Fasta  biojs-io0fasta for all the  other interaction with fasta file.
-
-var Fasta = require('biojs-io-fasta');
-
-
-
-module.exports = app;
--- lib/index.ts
+++ lib/index.ts
@@ -3,5 +3,5 @@
  */
 
 
-
+export * from "./FastaUtil";
 
